4e150bcc06bd72b5539260a9c5f8b11360dffea3
choi2601 <byeonghyeonchoe55@gmail.com>
2023-07-08 15:39:03 +0900

    sub execution-module context

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,11 +1,38 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
+  for (let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
 
-  products += `<div>${name}</div>`;
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// Running this file in development in isolation
+// Using local index.html file
+// Which DEFNITELY has an element with an id of 'dev-products'
+// want to immediately render app into that element
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  // Assuming container doen't have an element
+  // with id dev-products
+  if (el) {
+    // probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+// Context/Situation #2
+// Running this file in development or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products'
+// DO NOT WANT try to immediately render the app
+
+export { mount };
