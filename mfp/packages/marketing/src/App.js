69fc700e8075009ed9bca202bf461150ee60e206
choi2601 <byeonghyeonchoe55@gmail.com>
2023-07-12 21:59:40 +0900

    resolve confilct of classname in production

--- mfp/packages/marketing/src/App.js
+++ mfp/packages/marketing/src/App.js
@@ -1,14 +1,19 @@
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  // create generate a specific prefix name for resolce conflit css classname
+  productionPrefix: "ma",
+});
+
 export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
