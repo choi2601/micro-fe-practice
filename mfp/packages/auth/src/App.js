772e925dd2f4fb02dcdba0860331e4d07a8b9afd
choi2601 <byeonghyeonchoe55@gmail.com>
2023-07-17 14:51:50 +0900

    add publicPath for exactly load main.js file

--- mfp/packages/auth/src/App.js
+++ mfp/packages/auth/src/App.js
@@ -2,12 +2,12 @@ import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import { Switch, Route, Router } from "react-router-dom";
 
-import Landing from "./components/Landing";
-import Pricing from "./components/Pricing";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
   // create generate a specific prefix name for resolce conflit css classname
-  productionPrefix: "ma",
+  productionPrefix: "au",
 });
 
 export default ({ history }) => {
@@ -16,8 +16,8 @@ export default ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route exact path="/" component={Landing} />
+            <Route path="/auth/signin" component={Signin} />
+            <Route path="/auth/signup" component={Signup} />
           </Switch>
         </Router>
       </StylesProvider>
