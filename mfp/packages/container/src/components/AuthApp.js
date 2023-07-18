8d0bd5d783d1f7973b65c709182829bba225b12a
choi2601 <byeonghyeonchoe55@gmail.com>
2023-07-18 19:46:01 +0900

    add initialPath to memory history

--- mfp/packages/container/src/components/AuthApp.js
+++ mfp/packages/container/src/components/AuthApp.js
@@ -8,7 +8,7 @@ export default () => {
 
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
-      // location
+      initialPath: history.location.pathname,
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
